57ba75b2b40437163f0a4624aab20a0b2c23559f
Ryan Voots <simcop2387@simcop2387.info>
2016-10-15 02:57:33 -0700

    Fix glob and some other cases

--- testeval.sh
+++ testeval.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #echo "perl 1+1" | sudo `which perl` ./eval.pl
-echo "perl use experimental 'postderef';" | sudo strace -ostrace.log `which perl` ./eval.pl
+echo "perl [[glob '*'], [glob '{foo,bar}{baz,boob}']]" | sudo strace -ostrace.log `which perl` ./eval.pl
